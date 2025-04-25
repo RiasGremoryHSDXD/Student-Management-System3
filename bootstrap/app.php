43efa6928d933ff961be97b57b338278f496f9f9
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-04-25 20:50:29 +0800

    Edited the CROS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
+            \Illuminate\Http\Middleware\HandleCors::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
             HandleInertiaRequests::class,
-            \Illuminate\Http\Middleware\HandleCors::class,
         ]);
         
     })
